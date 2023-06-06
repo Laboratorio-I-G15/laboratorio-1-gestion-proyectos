af37d9898f511da232cf00c0a5fa2b9fb9871613
raffarraffa <lopezrafa@gmail.com>
2023-06-06 16:26:57 -0300

    Clases nuevas

---
+++ src/models/Equipo.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Equipo {
+
+    private int idEquipo;
+    private Poyecto proyecto;
+    private String nombre;
+    LocalDate creacion;
+    private int estado;
+}
