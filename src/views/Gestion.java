60a3e6f7642b72f05e1727f0f62b923861ef3943
Toloza  Leonel <112735110+LeoToloza1@users.noreply.github.com>
2023-06-08 22:38:49 -0300

    update 8/6
    se agregan la vista principal y la vista de los miembros

--- src/views/Gestion.java
+++ src/views/Gestion.java
@@ -10,14 +10,16 @@ package views;
  * @author leo_t
  */
 public class Gestion extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Gestion
-     */
+/** ----------DEFINICION DE VARIABLES GLOBALES----------
+ * Aca deberiamos definir las variables globales que va utilizar
+ * esta vista principal.
+ */
+  public static MiembrosViews vistaMiembro = null;
+    
     public Gestion() {
-        this.setLocationRelativeTo(null);
-        this.setTitle("Siste de Gestion de Proyectos");
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.setTitle("Sistema de Gestion de Proyectos");
     }
 
     /**
@@ -29,34 +31,68 @@ public class Gestion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDesktopPane1 = new javax.swing.JDesktopPane();
+        desktop = new javax.swing.JDesktopPane();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        adminMiembros = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout desktopLayout = new javax.swing.GroupLayout(desktop);
+        desktop.setLayout(desktopLayout);
+        desktopLayout.setHorizontalGroup(
+            desktopLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 1266, Short.MAX_VALUE)
         );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 682, Short.MAX_VALUE)
+        desktopLayout.setVerticalGroup(
+            desktopLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 661, Short.MAX_VALUE)
         );
+
+        adminMiembros.setText("Miembros");
+
+        jMenuItem1.setText("Administrar usuarios");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        adminMiembros.add(jMenuItem1);
+
+        jMenuItem2.setText("Asignar un equipo");
+        adminMiembros.add(jMenuItem2);
+
+        jMenuBar1.add(adminMiembros);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(desktop)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(desktop)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+          if (vistaMiembro == null) {
+            vistaMiembro = new MiembrosViews();
+            vistaMiembro.setVisible(true);
+             desktop.add(vistaMiembro);
+        }
+       
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -86,14 +122,17 @@ public class Gestion extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Gestion().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Gestion().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenu adminMiembros;
+    private javax.swing.JDesktopPane desktop;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }
