da803c968f830a687a7e4a96ad7816cec9276ee9
raffarraffa <lopezrafa@gmail.com>
2023-06-11 14:54:39 -0300

    Update EquipoMiembroData.java

--- src/controllers/EquipoMiembroData.java
+++ src/controllers/EquipoMiembroData.java
@@ -48,7 +48,7 @@ public class EquipoMiembroData {
      */
     public ArrayList<Miembro> selectEquipoMiembro(int id_equipo) {
         ArrayList<Miembro> miembros = new ArrayList();
-        String consulta = " SELECT `miembro`.`id_miembro`, `miembro`.`nombre`"
+        String consulta = " SELECT `miembro`.`apellido`,`miembro`.`id_miembro`, `miembro`.`nombre`, `miembro`.`dni`, `miembro`.`estado`"
                 + " FROM `miembro` JOIN `equipo_miembro` ON `miembro`.`id_miembro`=`equipo_miembro`.`id_miembro`"
                 + " JOIN `equipo` ON `equipo_miembro`.`id_equipo`=`equipo`.`id_equipo`"
                 + " WHERE `equipo`.`id_equipo` = ?;";
@@ -57,21 +57,18 @@ public class EquipoMiembroData {
             stmt.setInt(1, id_equipo);
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
-                while (result.next()) {
-                    Miembro miembro_1 = new Miembro();
-                    miembro_1.setIdMiembro(result.getInt("id_miembro"));
-                    miembro_1.setDni(result.getLong("dni"));
-                    miembro_1.setApellido(result.getString("apellido"));
-                    miembro_1.setNombre(result.getString("nombre"));
-                    miembro_1.setEstado(result.getBoolean("estado"));
-                    miembros.add(miembro_1);
-                }
+                Miembro miembro_1 = new Miembro();
+                miembro_1.setIdMiembro(result.getInt("id_miembro"));
+                miembro_1.setDni(result.getLong("dni"));
+                miembro_1.setApellido(result.getString("apellido"));
+                miembro_1.setNombre(result.getString("nombre"));
+                miembro_1.setEstado(result.getBoolean("estado"));
+                miembros.add(miembro_1);
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error: \n" + e.getMessage(), "Se ha producido un error.", JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
         }
-
         return miembros;
     }
 
@@ -83,25 +80,22 @@ public class EquipoMiembroData {
      */
     public ArrayList<Miembro> selectEquipoMiembro(Equipo equipo) {
         ArrayList<Miembro> miembros = new ArrayList();
-        String consulta = " SELECT `miembro`.`id_miembro`, `miembro`.`nombre`"
+        String consulta = " SELECT `miembro`.`apellido`,`miembro`.`id_miembro`, `miembro`.`nombre`, `miembro`.`dni`, `miembro`.`estado`"
                 + " FROM `miembro` JOIN `equipo_miembro` ON `miembro`.`id_miembro`=`equipo_miembro`.`id_miembro`"
                 + " JOIN `equipo` ON `equipo_miembro`.`id_equipo`=`equipo`.`id_equipo`"
                 + " WHERE `equipo`.`id_equipo` = ?;";
         try (PreparedStatement stmt = Conexion.getConexion().prepareStatement(consulta)) {
             // bindeo id_proyecto
-            
             stmt.setInt(1, equipo.getId_equipo());
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
-                while (result.next()) {
-                    Miembro miembro_1 = new Miembro();
-                    miembro_1.setIdMiembro(result.getInt("id_miembro"));
-                    miembro_1.setDni(result.getLong("dni"));
-                    miembro_1.setApellido(result.getString("apellido"));
-                    miembro_1.setNombre(result.getString("nombre"));
-                    miembro_1.setEstado(result.getBoolean("estado"));
-                    miembros.add(miembro_1);
-                }
+                Miembro miembro_1 = new Miembro();
+                miembro_1.setIdMiembro(result.getInt("id_miembro"));
+                miembro_1.setDni(result.getLong("dni"));
+                miembro_1.setApellido(result.getString("apellido"));
+                miembro_1.setNombre(result.getString("nombre"));
+                miembro_1.setEstado(result.getBoolean("estado"));
+                miembros.add(miembro_1);
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error: \n" + e.getMessage(), "Se ha producido un error.", JOptionPane.ERROR_MESSAGE);
