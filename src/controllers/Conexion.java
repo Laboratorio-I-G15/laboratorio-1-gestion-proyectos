463874a6b80c5eaf19256af22579a38561177080
raffarraffa <lopezrafa@gmail.com>
2023-06-19 20:58:33 -0300

    Correcciones menores

--- src/controllers/Conexion.java
+++ src/controllers/Conexion.java
@@ -22,9 +22,9 @@ public class Conexion {
      */
     private static Connection conectar = null;
     private static String base = "leotoloza_proyectogestion"; //nombre de la base de datos
-    private static String usuario ="leotoloza"; // usuario para acceder a la base de datos, por defecto es 'root'
-    private static String pass = "LeonelSantiagoToloza113346"; // contraseña para acceder, por defecto el usuario root, no tiene contraseña
-    private static String url = "jdbc:mysql://mysql-leotoloza.alwaysdata.net/" + base; // URL es la direccion de la Conexion, por defecto en local y el puerto 3306 + el nombre de la db
+    private static String usuario = "root"; // usuario para acceder a la base de datos, por defecto es 'root'
+    private static String pass = ""; // contraseña para acceder, por defecto el usuario root, no tiene contraseña
+    private static String url = "jdbc:mysql://localhost/" + base; // URL es la direccion de la Conexion, por defecto en local y el puerto 3306 + el nombre de la db
 //    private static String base = "leotoloza_proyectogestion"; //nombre de la base de datos
 //    private static String usuario = "leotoloza"; // usuario para acceder a la base de datos, por defecto es 'root'
 //    private static String pass = "LeonelSantiagoToloza113346"; // contraseña para acceder, por defecto el usuario root, no tiene contraseña
